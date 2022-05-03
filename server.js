1e41b3862359044e3a76b2cd3217896a03eda451
ashleyromano <ashleye.romano@gmail.com>
2022-05-03 19:59:11 -0400

    fixed branch issue

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
 import {router as ordersRouter} from './routes/orders.js'
 
-import('./config/database.js')
+import('./config/database.js') 
+
 
 const app = express()
 
